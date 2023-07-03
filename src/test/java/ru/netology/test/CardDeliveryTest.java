b1593792ebb94862e9bef0bb2a0be80c6e4e180a
Ilya Valikov <valikov.skeleton1977@yandex.ru>
2023-07-03 17:13:15 +0300

    fixed project

--- src/test/java/ru/netology/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/test/CardDeliveryTest.java
@@ -1,12 +1,8 @@
 package ru.netology.test;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
-import org.checkerframework.checker.units.qual.C;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Timeout;
 import org.openqa.selenium.Keys;
 import ru.netology.gen.CardGenerator;
 
@@ -28,18 +24,18 @@ class CardDeliveryTest {
     @DisplayName("Should successful plan and replain meeting")
     void shouldSuccessfulPlanAndReplanMeeting() {
         CardGenerator.UserInfo validUser = CardGenerator.Registration.generateUser("ru");
-        int daysToAddInFirstMeeting = 4;
+        int daysToAddInFirstMeeting = 10;
         String firstMeetingDate = CardGenerator.generateDate(daysToAddInFirstMeeting);
-        int daysToAddInReplain = 7;
+        int daysToAddInReplain = 13;
         String replainDate = CardGenerator.generateDate(daysToAddInReplain);
         $("[data-test-id=city] input").setValue(validUser.getCity());
-        $("[data-test-id=date] input").setValue(String.valueOf(daysToAddInFirstMeeting));
+        $(".calendar-input__custom-control input").doubleClick().sendKeys(firstMeetingDate);
         $("[data-test-id=name] input").setValue(validUser.getName());
         $("[data-test-id=phone] input").setValue(validUser.getPhone());
         $("[data-test-id=agreement]").click();
         $(byText("Запланировать")).click();
         $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
-        //$(byClassName("notification")).shouldHave(exactText("Встреча успешно запланирована на " + firstMeetingDate)).shouldBe(visible, Duration.ofSeconds(50));
+        $("[data-test-id=success-notification] .notification__content").shouldHave(text("Встреча успешно запланирована на " + firstMeetingDate)).shouldBe(visible);
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue(String.valueOf(replainDate));
         $(byText("Запланировать")).click();
